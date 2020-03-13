e488f82522190203f93154b15ac861a40f5f22e6
Shidong Wang <wsdjeg@outlook.com>
2020-03-13 21:46:05 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,18 @@ use std::process;
 fn main() {
     #[allow(unused_variables)]
     let args: Vec<String> = std::env::args().collect();
+    if args.len() == 2 {
+        if args[1] == String::from("-v") {
+            version();
+            process::exit(0);
+        }else if args[1] == String::from("-h") {
+            help();
+            process::exit(0);
+        }
+    }
     // let config = Config::new(&args).unwrap_or_else(|err| {
-        // println!("Problem parsing arguments: {}", err);
-        // process::exit(1);
+    // println!("Problem parsing arguments: {}", err);
+    // process::exit(1);
     // });
     let config = Config {
         query: String::from("ru"),
@@ -46,4 +55,15 @@ fn find_start(words: &String, query: &String) {
             println!("{}", word);
         }
     }
+}
+
+fn version() {
+    let version = "v0.1.0";
+    println!("minigrep: {}", version);
+}
+
+fn help() {
+    version();
+    println!("{}", "USAGE");
+    println!("{}", "      minigrep <query> <filename>");
 }

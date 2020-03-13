2b0fe5f28bfea67d25f74a528d618b328c5b3916
Shidong Wang <wsdjeg@outlook.com>
2020-03-13 20:46:41 +0800

    Improve config

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 use std::fs;
+use std::process;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let fname = &args[2];
-    let query = &args[1];
-    let words = fs::read_to_string(fname).expect("");
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    let words = fs::read_to_string(config.filename).expect("");
     for word in words.lines() {
-        if word.contains(query) {
+        if word.contains(&config.query) {
             println!("{}", word);
         }
+    }
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
     }
 }
